3c5ddf5797bcf4d702cb2b9d5556a44d82c5c169
Maximilian Aigner <maximilian.aigner@tutanota.de>
2020-04-23 11:02:19 +0200

    Added data import scripts

---
+++ data/handle_sql.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+import sys
+import json
+import sqlite3
+
+conn = sqlite3.connect('comments.sqlite')
+c = conn.cursor()
+
+i = 0
+for line in sys.stdin:
+  json_obj = json.loads(line)
+  if json_obj['author'] == '[deleted]':
+    continue
+  vals = [json_obj['created_utc'], json_obj['author'], json_obj['subreddit'], json_obj['score']]
+  c.execute("INSERT INTO comments VALUES (?,?,?,?)", vals)
+  if i % 100000 == 0: conn.commit()
+
+conn.commit()
+conn.close()
---
+++ data/pretreatment.sql
@@ -0,0 +1,14 @@
+delete from comments where author like "%bot" or author == "AutoModerator" or subreddit like "u\_-%" escape '\';
+delete from comments 
+  where author in
+    (select author from comments 
+     group by author
+     having count(distinct subreddit) < 3)
+  or subreddit in
+    (select subreddit from comments
+     group by subreddit
+     having count(distinct author) < 10);
+.mode csv
+.output cleaned.csv
+select * from comments;
+.quit
