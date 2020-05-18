441f1eca4b4d6f5f7ed7d97358cc538b391a26d5
Maximilian Aigner <maximilian.aigner@tutanota.de>
2020-05-18 19:19:58 +0200

    Updates to community detection / collaborative filtering methods
    - Revamped NMF, using rsparse package and applied to the tf-idf weighted data
    - Added fast SVD

--- R/community-detection.R
+++ R/community-detection.R
@@ -3,13 +3,28 @@
 library(Matrix)
 library(lda.svi)
 library(tibble)
-library(NMFN)
+library(rsparse)
+library(irlba)
 
 posts_crosstab <- xtabs(~ author + subreddit, df, sparse = TRUE)
 
 # For other methods, it is convienent to have (i, j)-indexing
 # rather than the default (i, p) index in dgCmatrix
 posts_crosstab <- as(posts_crosstab, "TsparseMatrix")
+crosstab_tfidf <- log1p(posts_crosstab)
+idfs <- log( ncol(posts_crosstab) / rowSums(posts_crosstab > 0))
+crosstab_tfidf <- crosstab_tfidf * idfs
+
+##############################
+# Singular Value Decomposition
+##############################
+
+# Classic SVD decomposition of the TF-IDF weighted matrix
+# Compute the TF-IDF weighted matrix
+
+ssvd <- irlba(crosstab_tfidf, nv = 100, verbose = TRUE)
+rownames(ssvd$u) <- dimnames(posts_crosstab)[["author"]]
+rownames(ssvd$v) <- dimnames(posts_crosstab)[["subreddit"]]
 
 #############################
 # Latent Dirichlet Allocation
@@ -24,13 +39,26 @@ lda_topics <- dcast(lda$theta, document ~ topic) %>% column_to_rownames('documen
 # Non-Negative Matrix Factorisation
 ###################################
 
-# Scale by user, since we don't care about reproducing high numbers
-# in the high-traffic subreddits; leaving the post counts
-# will overweight high-traffic users / subreddits
-posts_crosstab <- posts_crosstab / rowSums(posts_crosstab)
+# Use tf-idf scaling, since:
+# - we don't care about reproducing high numbers in the high-traffic
+#   subreddits; leaving the post counts will overweight
+#   high-traffic users / subreddits
+# - higher information content per matrix entry
 
-# Need this since nnmf requires `matrix` type
-class(posts_crosstab) <- "matrix"
 
-pnnmf <- nnmf(posts_crosstab, 10)
-nnmf_topics <- t(pnnmf$H)
+wrmf <- WRMF$new(rank = 50, non_negative = TRUE)
+W <- wrmf$fit_transform(crosstab_tfidf, 25, 0.001)
+H <- wrmf$components
+
+# Recommendations
+recommend_sub <- function(name, k = 20) {
+  myq <- H[, name, drop = FALSE]
+  sims <- text2vec::sim2(t(myq), t(H), method = "cosine")[1,]
+  return(head(sort(sims, decreasing = TRUE), k))
+}
+
+recommend_user <- function(uname, k = 20) {
+  myq <- t(W[uname,])
+  sims <- text2vec::sim2(as.matrix(myq), W, method = "cosine")[1,]
+  return(head(sort(sims, decreasing = TRUE), k))
+}
